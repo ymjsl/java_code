38e1ea0a6cddc438b7f88cd5c5e96873662ce607
ymjsl <141890844+ymjsl@users.noreply.github.com>
2023-11-14 19:43:15 +0800

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+
+public class helloworld {
+
+     public static void main(String[] args) {
+     	System.out.println("hello");
+     }
+}
+class helloworld2 {
+
+     public static void main(String[] args) {
+          System.out.println("hello");
+     }
+}
