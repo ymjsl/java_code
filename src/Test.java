140a73485e6af260c7e37319a7ce4a5decd61dea
ymjsl <141890844+ymjsl@users.noreply.github.com>
2023-11-16 20:24:32 +0800

    Add files via upload

--- src/Test.java
+++ src/Test.java
@@ -2,45 +2,101 @@
  * Created with IntelliJ IDEA.
  * Description:
  * User: admin
- * Date: 2023-11-15
- * Time: 18:53
+ * Date: 2023-11-16
+ * Time: 10:38
  */
-//psvm
-//sout
-//fori
-//标识符不能由数字开头 main 可以做标识符，但最好别用
 public class Test {
-
     public static void main(String[] args) {
-        //int a = 10;
-        //System.out.println(a);
-        //System.out.println(10);
-        //System.out.println(10.5);
-        //System.out.println(Integer.MAX_VALUE);
-        //System.out.println(Integer.MIN_VALUE);
-        long a=10L;
+        String str ="1234";
+        int a=Integer.parseInt(str);
+        System.out.println(a+1);
+        int b=Integer.valueOf(str);
+        System.out.println(b+1);
+    }
+    public static void main10(String[] args) {
+        int a=10;
+        String str=a+"";
+        System.out.println(str);
+        //借助方法
+        String ret=String.valueOf(10);
+        System.out.println(ret);
+    }
+    public static void main2(String[] args) {
+        String s1="hello";
+        String s2="world";
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+        System.out.println("a:"+a +" b:"+b);
+        //                  a: 10    b: 20
+        //当字符串和整数在一起使用+号的时候 就是拼接的意思
+        System.out.println("a: "+a + b);
+        //                  a:  1020
+        System.out.println(a+b+"a: b:");
+        //                 30   a: b:
+        System.out.println(s1+s2);//表示拼接
+    }
+    public static void main1(String[] args) {
+        byte a=10;
+        byte b=20;
+        //当你参与运算的数据 小于4个字节的时候 ，会进行提升（byte,short,char
+        //byte c=a+b;
+        int c=a+b;
+    }
+    public static void main7(String[] args) {
+        int a=10;
+        long b=20;
+        //int c=a+b;//整型提升为long,不能小转大
+        int c=(int)(a+b);
+        char m=10;
+        char n=20;
+        //char k=m+n; erroneous
+    }
+    public static void main5(String[] args) {
+        int a=97;
+        char ch=(char)a;
+        double d = 97.8;
+        char ch2 = (char)d;
+        System.out.println(ch);
+        System.out.println(ch2);
+    }
+    public static void main4(String[] args) {
+        int a=10;
+        long b=100L;
+        //b=a; // 隐式类型转换
+        //a=b erroneous
+        a=(int)b;
         System.out.println(a);
-        System.out.println(Long.MAX_VALUE);//long 的包装类型 Long
-        System.out.println(Short.MAX_VALUE);//short 的包装类型 Short
-        byte b=10;
-        
     }
-
-    /*public static void func(String[] args) {
-        //Integer -> 把他当作 int plus
-        System.out.println(Integer.MAX_VALUE);//int 最大值
+    public static void main3(String[] args) {
+        boolean b=true;
+        System.out.println(b);
     }
-
-    public static void func2(String[] args) {
-
-    }*/
 }
-//数据类型(8个）
-//外存 ：磁盘 光盘 U盘
-//内存:RAM
-//boolean 布尔类型
-//byte 字节类型 1
-//char 2
-//任何位的操作系统，字节不变，可移植性很高
-//在java中 没有所谓的 无符号数字
-
+    /*public static void main2(String[] args) {
+        *//*byte b=10;
+        System.out.println(b);
+        double d=12.5;//双精度浮点型
+        System.out.println(d);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        float f=12.5f;
+        System.out.println(f);*//*
+        int a=1;
+        int b=2;
+        System.out.println((double)a/b);
+        double d=1.1;
+        System.out.println(d*d);
+        System.out.println(Character.MAX_VALUE);
+    }
+}*/
+//c语言 ：ASCII字符集
+//java: Unicode字符集
+//布尔型变量 只有true 或 false
+//无明确大小
+//int boolean 不能相互转换
+//int  Integer
+//char  Character
+//小转大可以
+//大转小要强转  不相关的不能转
+//字符串类型 String
