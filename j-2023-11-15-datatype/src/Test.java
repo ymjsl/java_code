942bf47fe1f65efc2316ac5796be20a184cc5d24
ymjsl <141890844+ymjsl@users.noreply.github.com>
2023-11-20 11:20:57 +0800

    Add files via upload

---
+++ j-2023-11-15-datatype/src/Test.java
@@ -0,0 +1,46 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: admin
+ * Date: 2023-11-15
+ * Time: 18:53
+ */
+//psvm
+//sout
+//fori
+//标识符不能由数字开头 main 可以做标识符，但最好别用
+public class Test {
+
+    public static void main(String[] args) {
+        //int a = 10;
+        //System.out.println(a);
+        //System.out.println(10);
+        //System.out.println(10.5);
+        //System.out.println(Integer.MAX_VALUE);
+        //System.out.println(Integer.MIN_VALUE);
+        long a=10L;
+        System.out.println(a);
+        System.out.println(Long.MAX_VALUE);//long 的包装类型 Long
+        System.out.println(Short.MAX_VALUE);//short 的包装类型 Short
+        byte b=10;
+        
+    }
+
+    /*public static void func(String[] args) {
+        //Integer -> 把他当作 int plus
+        System.out.println(Integer.MAX_VALUE);//int 最大值
+    }
+
+    public static void func2(String[] args) {
+
+    }*/
+}
+//数据类型(8个）
+//外存 ：磁盘 光盘 U盘
+//内存:RAM
+//boolean 布尔类型
+//byte 字节类型 1
+//char 2
+//任何位的操作系统，字节不变，可移植性很高
+//在java中 没有所谓的 无符号数字
+
