ef0bf2d66722e04f930e52c55ef5765f34a8042f
ymjsl <141890844+ymjsl@users.noreply.github.com>
2023-11-20 11:20:38 +0800

    Add files via upload

---
+++ test_2023_11_14/src/HelloWorld.java
@@ -0,0 +1,32 @@
+
+public class HelloWorld {
+
+     public static void main(String[] args){
+//         System.out.println("Hello World!!");
+     }
+}
+// -encoding utf-8  设置编码为utf-8
+//javadoc -d myHello -author -version -encoding UTF-8 -charset UTF-8 HelloWorld.java
+//           目录名
+/**
+ * 文档注释
+ */
+//快捷键
+//psvm
+//sout
+//fori
+//块注释 ：ctrl+shift+/
+//行注释：ctrl+/
+class Test{
+    //java的数组定义方式：int[] array
+    //java中，函数即方法
+    // 标识符 ：在程序中由用户给类名、方法名、或变量所取的名字
+
+    public static void main(String[] args) {
+        /*System.out.println("nihao mjsl");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("sb");
+        }
+        int a = 10_0000;*/
+    }
+}
